da1ca6d052dba50b54950a53d71562d50b3851fd
sandrosaric <sandro.saric@gmail.com>
2022-12-06 18:34:05 +0100

    Added walking skeleton - Angular

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
@@ -18,6 +19,14 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => {
+    builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200")
+    .WithOrigins("https://localhost:4200");
+}   
+);
 
 app.MapControllers();
 
